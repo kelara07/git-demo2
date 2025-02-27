258bd4dd8c8d5351ab47a18efe2c0f4276f13014
klaraliu <klaraliu@gouptech.com>
2025-02-27 17:14:14 +0800

    新增rules

--- MyDemo2/.eslintrc.cjs
+++ MyDemo2/.eslintrc.cjs
@@ -9,6 +9,22 @@ module.exports = {
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'off',
+    'no-empty': 'off',
+    'vue/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'vue/no-mutating-props': [
+      'error',
+      {
+        shallowOnly: true
+      }
+    ],
+    // 对全角空格，仅作提醒。
+    'no-irregular-whitespace': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'error'
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
